91c3d278b18486d43f62e307a7af2e6b90fb1736
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2017-05-27 06:24:03 +0700

    meta viewport

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>List User</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
